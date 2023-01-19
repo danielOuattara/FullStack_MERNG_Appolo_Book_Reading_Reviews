26fd923e1dc285132a2f891ea627dbe3318de828
danielOuattara <daniel.ouattara@gmx.com>
2023-01-20 00:23:25 +0100

    adding author query

--- appolo_server/server.js
+++ appolo_server/server.js
@@ -2,6 +2,18 @@ import { ApolloServer } from "@apollo/server";
 
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+// dummy data
+const books = [
+  { title: "Name of the wind", genre: "Fantasy", id: "1", pages: "230" },
+  { title: "The final empire", genre: "Fantasy", id: "2", pages: "340" },
+  { title: "The long earth", genre: "Sci-Fi", id: "3", pages: "120" },
+];
+const authors = [
+  { name: "Patrick Rothfus", age: "44", id: "1" },
+  { name: "Bradon Sanderso", age: "42", id: "2" },
+  { name: "Terry Pratchett", age: "66", id: "3" },
+];
+
 const typeDefs = `#graphql
 
   type Book {
@@ -10,31 +22,29 @@ const typeDefs = `#graphql
     genre: String
     pages: Int
   }
+  type Author {
+    id: ID
+    name: String
+    age: Int
+  }
 
   type Query {
     books: [Book]
     book(id: ID!): Book
+    authors: [Author]
+    author(id: ID!): Author
   }
 `;
-
-// dummy data
-const books = [
-  { title: "Name of the wind", genre: "Fantasy", id: "1", pages: "230" },
-  { title: "The final empire", genre: "Fantasy", id: "2", pages: "340" },
-  { title: "The long earth", genre: "Sci-Fi", id: "3", pages: "120" },
-];
-
-const authors = [
-  { name: "Patrick Rothfus", age: "44", id: "1" },
-  { name: "Bradon Sanderso", age: "42", id: "2" },
-  { name: "Terry Pratchett", age: "66", id: "3" },
-];
 
 const resolvers = {
   Query: {
     books: () => books,
     book: (parent, arg, ctx) => {
       return books.find((book) => book.id === arg.id);
+    },
+    authors: () => authors,
+    author: (parent, arg, ctx) => {
+      return authors.find((author) => author.id === arg.id);
     },
   },
 };
