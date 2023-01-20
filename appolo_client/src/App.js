bc77f33b7b9a1655ff752fcbf7100dae02660abb
danielOuattara <daniel.ouattara@gmx.com>
2023-01-20 11:22:32 +0100

    initial config for appolo client OK

--- appolo_client/src/App.js
+++ appolo_client/src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import BookList from "./components/BookList";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
+// client
+//   .query({
+//     query: gql`
+//       #graphql
+//       query GetAuthors {
+//         authors {
+//           id
+//           name
+//           age
+//         }
+//       }
+//     `,
+//   })
+//   .then((result) => console.log("result = ", result));
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ApolloProvider client={client}>
+      <BookList />
+    </ApolloProvider>
+  );
 }
 
 export default App;
