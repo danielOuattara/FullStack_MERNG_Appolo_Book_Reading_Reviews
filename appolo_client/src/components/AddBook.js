2eca2f92730a1170d2a67a52df6b55efe0303112
danielOuattara <daniel.ouattara@gmx.com>
2023-01-20 12:05:12 +0100

    creating form: capture user input in AddBook comp

--- appolo_client/src/components/AddBook.js
+++ appolo_client/src/components/AddBook.js
@@ -1,7 +1,26 @@
 import { useQuery } from "@apollo/client";
 import { GET_AUTHORS } from "./../queries/queries";
+import { useState } from "react";
+//----------------------------------------------------------
 
 export default function AddBook() {
+  const [newBook, setNewBook] = useState({
+    title: "",
+    genre: "",
+    authorId: "",
+  });
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    console.log("newBook = ", newBook);
+    setNewBook((prevState) => ({
+      ...prevState,
+      title: "",
+      genre: "",
+      authorId: "",
+    }));
+  };
+
   const { loading, error, data } = useQuery(GET_AUTHORS);
 
   if (loading) return <p>Loading...</p>;
@@ -16,24 +35,56 @@ export default function AddBook() {
   };
 
   return (
-    <form id="add-book">
+    <form id="add-book" onSubmit={submitForm}>
       <div className="field">
         <label htmlFor="bookName">
           Book Name :
-          <input type="text" id="bookName" name="bookName" />
+          <input
+            type="text"
+            id="bookName"
+            name="bookName"
+            value={newBook.title}
+            onChange={(event) =>
+              setNewBook((prevState) => ({
+                ...prevState,
+                title: event.target.value,
+              }))
+            }
+          />
         </label>
       </div>
 
       <div className="field">
         <label htmlFor="genre">
           Genre :
-          <input type="text" id="genre" name="genre" />
+          <input
+            type="text"
+            id="genre"
+            name="genre"
+            value={newBook.genre}
+            onChange={(event) =>
+              setNewBook((prevState) => ({
+                ...prevState,
+                genre: event.target.value,
+              }))
+            }
+          />
         </label>
       </div>
 
       <div className="field">
         <label htmlFor="author">Author : </label>
-        <select name="author" id="author">
+        <select
+          name="author"
+          id="author"
+          value={newBook.authorId}
+          onChange={(event) =>
+            setNewBook((prevState) => ({
+              ...prevState,
+              authorId: event.target.value,
+            }))
+          }
+        >
           <option value="">Select Author </option>
           {displayAuthors()}
         </select>
